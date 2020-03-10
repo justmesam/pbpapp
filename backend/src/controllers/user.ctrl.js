edb48f893bc3876bcd8b42c4667be81e5b57389d
justmesam <sthiongo12@gmail.com>
2020-03-10 15:48:18 +0300

    Add a Fetch current user route

--- backend/src/controllers/user.ctrl.js
+++ backend/src/controllers/user.ctrl.js
@@ -165,5 +165,41 @@ module.exports = {
     return res
       .status(400)
       .send({ message:`Kindly provide items to be updated`});
+  },
+  FetchUser: async (req, res) => {
+    const userId = res.locals.userId;
+    let shop;
+
+    const user = await User.findOne({
+      _id: userId
+    });
+
+    shop = await Shop.findOne({
+      vendor: userId
+    });
+
+
+    const shopDetails = shop ? {
+      id: shop._id,
+      name: shop.name,
+      longitude: shop.longitude,
+      latitude: shop.latitude,
+      vendor: shop.vendor,
+      dateCreated: shop.dateCreated
+    } : {}
+
+
+    const responseObject = {
+      email: user.email,
+      username: user.username,
+      id: user._id,
+      isVendor: user.isVendor,
+      dateJoined: user.dateJoined,
+      shop: shopDetails
+    }
+
+    return res.send({
+      user : responseObject
+    })
   }
 };
